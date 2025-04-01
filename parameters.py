6cb8cbe168db91165153805ae76a191d4e6cb964
0skyq <sankshayreddy47@gmail.com>
2025-04-01 17:24:43 +0530

    first commit

--- parameters.py
+++ parameters.py
@@ -1,4 +1,4 @@
-CRED_PATH = r"C:\Users\sanks\Documents\SHERLOCK HOLMES\Mtech Files\Soil_NPK_ML\fir-demo-22684-firebase-adminsdk-er67w-2e407deab9.json"
+CRED_PATH = r"C:\Users\sanks\Documents\SHERLOCK HOLMES\Mtech Files\fir-demo-22684-firebase-adminsdk-er67w-2e407deab9.json"
 URL = 'https://fir-demo-22684-default-rtdb.firebaseio.com'
 
 INITIALIZATION_PATH = 'Initialization'
